d995289db3515ba35343767658dcfd8c4d3decef
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-01-25 17:34:12 -0500

    Create mailcow-pre.sh

---
+++ usr/bin/mailcow-pre.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+set -ex
+
+FOLDER="/var/opt/persist/mailcow"
+[ ! -d "$FOLDER"/data/assets/ssl ] && mkdir -p "$FOLDER"/data/assets/ssl
+
+find "$FOLDER"/data | grep 'ssl-example/dhparams.pem$' | while IFS= read -r i; do
+    sudo cp "$i" "$FOLDER"/data/assets/ssl/dhparams.pem
+done
+
+find /etc/ssl | grep "$(cat /etc/WEBSITE)" | grep 'fullchain.pem$' | sort | tail -n 1 | while IFS= read -r i; do
+    sudo cp "$i" "$FOLDER"/data/assets/ssl/cert.pem
+done
+
+find /etc/ssl | grep "$(cat /etc/WEBSITE)" | grep 'privkey.pem$' | sort | tail -n 1 | while IFS= read -r i; do
+    sudo cp "$i" "$FOLDER"/data/assets/ssl/key.pem
+done
+
+chown 1000:1000 "${FOLDER}"
+exit 0
