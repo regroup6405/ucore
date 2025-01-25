46f4872c0b1943ada79e2b7166230bceb3678500
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-01-24 20:33:38 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 
 dnf install -y htop
 
+systemctl enable docker.service
 systemctl enable portaineragent.service
 
 find /usr/bin -type f | grep '\.sh$' | sort | while IFS= read -r i; do
