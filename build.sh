c13d465977185ccf0fb2d5cededb7ef0680c6448
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-01-24 21:52:52 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ dnf install -y htop
 
 systemctl enable docker.service
 systemctl enable portaineragent.service
+systemctl enable autoreboot.timer
 
 find /usr/bin -type f | grep '\.sh$' | sort | while IFS= read -r i; do
   chmod +x "$i"
