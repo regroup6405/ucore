3e2c6cee3f72b3644686782913909a88fa695ce9
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-03-09 12:22:12 -0400

    Update zfs-snap-send.sh

--- usr/bin/zfs-snap-send.sh
+++ usr/bin/zfs-snap-send.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #set -x
+ROOTDATASTORE="${1}"
 
 ZFSNAP="$(whereis zfsnap | awk '{print $2}')"
 [ ! -f "$ZFSNAP" ] && exit 1
 
-sudo "$ZFSNAP" destroy -r "persist"
+sudo "$ZFSNAP" destroy -r "${ROOTDATASTORE}"
 
 zfs list \
   | grep -v "^NAME" \
@@ -13,14 +14,14 @@ zfs list \
       sudo zfs set refreservation=none "$i"
     done
 
-sudo "$ZFSNAP" snapshot -a 3d -r "persist"
+sudo "$ZFSNAP" snapshot -a 3d -r "${ROOTDATASTORE}"
 
 SOURCELIST="$(zfs list -t snap | grep @)"
 TARGETDATASETS="$(ssh -n ${TARGETSSH} 'zfs list' | awk '{print $1}' | grep '/')"
 TARGETLIST="$(ssh -n ${TARGETSSH} 'zfs list -t snap' | grep '@')"
 
 echo "${SOURCELIST}" \
-| grep -v 'persist@' \
+| grep -v "${ROOTDATASTORE}@" \
 | cut -d'@' -f1 \
 | sort \
 | uniq \
@@ -31,16 +32,15 @@ echo "${SOURCELIST}" \
       ssh -n ${TARGETSSH} "sudo zfs create $i"
     fi
     EXISTSONTARGET="$(echo "$TARGETLIST" | grep -q "$i" && echo 1)"
+    ssh -n "$TARGETSSH" sudo zfs set readonly=off "$i"
     if [ "$EXISTSONTARGET" == "1" ]; then
       echo "$i exists on target, incrementally sending..."
       TARGETLATEST="$(echo "$TARGETLIST" | grep "$i" | tail -n 1 | awk '{print $1}')"
       [ "$SOURCELATEST" == "$TARGETLATEST" ] && echo "$TARGETLATEST == $SOURCELATEST, continuing..." && continue
       echo "$TARGETLATEST > $SOURCELATEST"
-      ssh -n "$TARGETSSH" sudo zfs set readonly=off "$i"
       echo sudo zfs send -i "$TARGETLATEST" "$SOURCELATEST" \| ssh ${TARGETSSH} sudo zfs recv $i | bash
     else
       echo "$i doesn't exist on target, sending from scratch..."
-      ssh -n "$TARGETSSH" sudo zfs set readonly=off "$i"
       SOURCEFIRST="$(echo "$SOURCELIST" | grep "$i" | head -n 1 | awk '{print $1}')"
       echo sudo zfs send "$SOURCEFIRST" \| ssh ${TARGETSSH} sudo zfs recv -F $i | bash
       echo sudo zfs send -i "$SOURCEFIRST" "$SOURCELATEST" \| ssh ${TARGETSSH} sudo zfs recv -F $i | bash
