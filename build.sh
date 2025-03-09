c96554c5ae8ef41ea32b50daf1b2f68cd3a12a43
regroup6405 <148574850+regroup6405@users.noreply.github.com>
2025-03-09 10:46:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,13 @@ autoreboot.timer
 docker-cleanup.timer
 EOF
 
+git clone https://github.com/zfsnap/zfsnap.git /usr/src/zfsnap
+cp /usr/src/zfsnap/sbin/zfsnap.sh /usr/sbin/zfsnap
+cp /usr/src/zfsnap/man/man8/zfsnap.8 /usr/share/man/man8/zfsnap.8
+cp -r /usr/src/zfsnap/share /usr
+chmod +x /usr/sbin/zfsnap
+rm -rf /usr/src/zfsnap
+
 cat "$TMP" | grep -v "^#" | grep '\.service$\|\.timer' | while IFS= read -r i; do
   systemctl enable "$i"
 done
